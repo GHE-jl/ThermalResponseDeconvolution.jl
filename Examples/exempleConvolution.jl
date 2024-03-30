94b70e38edc18c173509776b682b49c25e544fbe
GabrielDion <dion.gabriel100@gmail.com>
2024-03-30 10:37:37 -0400

    Continue to update the package.

--- Examples/exempleConvolution.jl
+++ Examples/exempleConvolution.jl
@@ -5,7 +5,7 @@ In Julia, this script can be called from the project folder.
 
 include("../Functions/Convolution.jl")
 
-using CSV, DataFrames, Plots, LaTeXStrings
+using CSV, Plots
 
 # Import synthetic data
 DATA = CSV.read("Data/Data.csv", DataFrame)
@@ -19,12 +19,12 @@ g = G[:, 2]
 # Convolution algorithm
 f = diff([0; T_in - T_out])
 
-Time = @time begin T_tmp = Convolution(f, g) end #todo: 
+Time = @time T_tmp = Convolution(f, g) #todo: 
 T_conv = T_tmp.+T_out[1]
 
 # Print results
 println("Temperature RMSE: $round(sqrt(mean((T_conv.-T_out).^2)), 2) [degC]")
-println("Convolution time: $(Time) [s]")
+# println("Convolution time: $(Time) [s]")
 
 # Plot results
 plot(t/3600/24, T_out, linewidth=1.5, linestyle=:solid, linecolor=:black, label="Reference")
@@ -32,4 +32,3 @@ plot!(t/3600/24, T_conv, linewidth=1.5, linestyle=:dash, linecolor=:cyan, label=
 plot!(framestyle=:box, grid=false, xlabel="Time (d)", ylabel="Temperature (°C)",
 xlabelfontsize=11, ylabelfontsize=11, xtickfontsize=11, ytickfontsize=11,
 legendfontsize=11)
-savefig("ConvolutionPlot.pdf")
--- Functions/Convolution.jl
+++ Functions/Convolution.jl
@@ -1,6 +1,6 @@
 using FFTW
 
-function Convolution(f::Vector{T}, g::Vector{T}) where T
+function convolution(f::Vector, g::Vector)
     # Perform a stationary convolution with fft and ifft with 2xn -1zero padding to avoid circular convolution.
 
     # Initialization
@@ -8,7 +8,7 @@ function Convolution(f::Vector{T}, g::Vector{T}) where T
     Pad = 2*n-1
 
     # Perform non-circular convolution in the spectral domain using padded signals
-    y = real(ifft(fft([f; zeros(T, Pad-n)]).*fft([g; zeros(T, Pad-n)])))
+    y = real(ifft(fft([f; zeros(Pad-n)]).*fft([g; zeros(Pad-n)])))
 
     # Verification of the presence of complex or NaN values in the output
     if any(isnan.(y)) || any(imag.(y) .!= 0)
