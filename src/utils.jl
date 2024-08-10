62cd7ee120b5edbbf382aa72c4b0521015d1b31e
GabrielDion <75374140+Gabriel-Dion@users.noreply.github.com>
2024-08-09 20:49:47 -0400

    Update rms in utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -2,7 +2,7 @@
 Script that includes some general functions used throughout the pagkage.
 """
 
-function rms(x::AbstractVector{T} where T<:Number)
+function rms(x::Vector{T} where T<:Real)
     rmse = sqrt(sum(y^2 for y in x) / length(x))
     return rmse
 end
